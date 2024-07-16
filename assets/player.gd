d315af77d72e951ef12c30ba5ecbb6c8512a3c8b
TheoLomeuBraga <theolomeu@gmail.com>
2024-07-16 11:41:09 -0300

    a

---
+++ assets/player.gd
@@ -0,0 +1,43 @@
+extends CharacterBody3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+
+var jump_power = 1200.0
+const GRAVITY = 980.2
+var jump_current_power = 0.0
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var move_input := Vector3.ZERO
+	move_input.x = Input.get_axis("left","right")
+	move_input.z = Input.get_axis("foward","back")
+	
+	if $RayCast3D.is_colliding():
+		velocity = $RayCast3D.get_collision_normal().direction_to(move_input.normalized())  * 1200.0 * delta
+	else:
+		velocity = move_input.normalized() * 1200.0 * delta
+	
+	var hit_floor = $ShapeCast3Dfloor.is_colliding()
+	if hit_floor:
+		jump_current_power = 0
+		if jump_current_power > 0:
+			$ShapeCast3Dceling.enabled = true
+		if Input.get_action_strength("jump") > 0.0:
+			jump_current_power = jump_power
+	
+	var hit_celing = $ShapeCast3Dceling.is_colliding()
+	if hit_celing:
+		$ShapeCast3Dceling.enabled = false
+		jump_current_power = 0
+	
+	if jump_current_power != 0:
+		velocity.y = jump_current_power * delta
+	
+	move_and_slide()
+	
+	jump_current_power -= delta * GRAVITY
+

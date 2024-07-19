716576ec35de8887906baa2e2ee7b92032190c53
TheoLomeuBraga <theolomeu@gmail.com>
2024-07-19 16:42:49 -0300

    a

---
+++ assets/configiration menu/configuration_menu.gd
@@ -0,0 +1,47 @@
+extends Control
+
+
+
+var redy_next_input = false
+func _input(event):
+	
+	if Input.is_action_just_pressed("ui_tab_right") == false && Input.is_action_just_pressed("ui_tab_left") == false:
+		redy_next_input = true
+	
+	if redy_next_input:
+		if Input.is_action_just_pressed("ui_tab_right"):
+			$TabContainer.select_next_available()
+			redy_next_input = false
+			
+		if Input.is_action_just_pressed("ui_tab_left"):
+			$TabContainer.select_previous_available()
+			redy_next_input = false
+
+
+var next_tab = 0
+func _process(delta):
+	if next_tab == 0:
+		$TabContainer/video/VBoxContainer/full_screen.grab_focus()
+		next_tab = -1
+		
+	elif next_tab == 1:
+		$TabContainer/audio/VBoxContainer/volume.grab_focus()
+		next_tab = -1
+		
+	elif next_tab == 2:
+		$TabContainer/controls/VBoxContainer/mouse_sensitivity.grab_focus()
+		next_tab = -1
+		
+
+func _on_tab_container_tab_selected(tab):
+	next_tab = tab
+
+func _on_full_screen_toggled(toggled_on):
+	if toggled_on:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+	
+
+
+
--- assets/main_menu/menus.gd
+++ assets/main_menu/menus.gd
@@ -11,20 +11,29 @@ func start_menu_on(on):
 	$start_menu.set_process(on)
 	$start_menu.visible = on
 	
+func configuration_menu_on(on):
+	if on:
+		add_child(load("res://assets/configiration menu/configuration_menu.tscn").instantiate())
+	else:
+		remove_child($configuration_menu)
+	
 
 @export var menu_type = 0
 func select_menu_type():
 	if menu_type == 0:
 		main_menu_on(true)
 		start_menu_on(false)
+		configuration_menu_on(false)
 		$main_menu/play.grab_focus()
 	elif menu_type == 1:
 		main_menu_on(false)
 		start_menu_on(true)
+		configuration_menu_on(false)
 		$start_menu/continue_.grab_focus()
 	elif menu_type == 2:
 		main_menu_on(false)
 		start_menu_on(false)
+		configuration_menu_on(true)
 		$start_menu/continue_.grab_focus()
 	
 
@@ -43,10 +52,15 @@ func _on_new_game_pressed():
 	get_tree().change_scene_to_file("res://testes cenas/city level.tscn")
 
 func _on_configurations_pressed():
-	pass # Replace with function body.
+	menu_type = 2
+	select_menu_type()
 
 func _on_exit_pressed():
 	get_tree().quit()
 
-
+func _process(delta):
+	if Input.get_action_strength("ui_cancel") > 0.0:
+		menu_type = 1
+		select_menu_type()
+		
 

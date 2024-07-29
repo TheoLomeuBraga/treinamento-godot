bae538edb1aa97f81181da6b96819e3c85fc42a3
Théo <63592879+TheoLomeuBraga@users.noreply.github.com>
2024-07-28 23:00:47 -0300

    a

--- assets/chekinpoint/chekinpoint.gd
+++ assets/chekinpoint/chekinpoint.gd
@@ -1,14 +1,10 @@
 extends Area3D
 
-var keep_emiting_timer = 0
-func _process(delta):
-	$GPUParticles3D.emitting = keep_emiting_timer > 0
-	if keep_emiting_timer > 0:
-		keep_emiting_timer -= delta
+
 
 func _on_body_entered(body):
 	if body.has_method("set_last_safe_pos"):
 		if body.hit_floor:
 			body.set_last_safe_pos()
-			keep_emiting_timer = 0.5
+			$GPUParticles3D.emitting = true
 			$AudioStreamPlayer.play()
--- assets/loading_screen/loading_screen.gd
+++ assets/loading_screen/loading_screen.gd
@@ -1,15 +1,16 @@
 extends Control
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	ResourceLoader.load_threaded_request(Global.current_scene_loading)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	Global.variables["pause"] = false
 	var progress := []
 	ResourceLoader.load_threaded_get_status(Global.current_scene_loading,progress)
 	$ProgressBar.value = progress[0] * 100
-	get_tree().change_scene_to_file(Global.current_scene_loading)
+	if progress[0] == 1:
+		get_tree().change_scene_to_file(Global.current_scene_loading)
+	
+	
